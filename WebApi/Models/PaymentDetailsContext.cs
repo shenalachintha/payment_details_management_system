7af8d52b87afbc63d7744a8a4e75bf207c0ba33b
shenalachintha <156101606+shenalachintha@users.noreply.github.com>
2025-09-19 19:37:35 +0530

    changes

---
+++ WebApi/Models/PaymentDetailsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected PaymentDetailsContext()
+        {
+        }
+        public DbSet<PaymentDetail> PaymentDetails{ get; set; }
+    }
+}
